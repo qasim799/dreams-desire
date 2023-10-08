3b4e77531e813fc46bb2c17bb4806df9414b402c
qasim799 <qasim799@gmail.com>
2023-10-09 01:13:50 +0500

    ll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <header className=" w-full bg-base-300 p-5 items-center flex justify-between">
       <main>
-        <span>Jawad Ka Kotha</span>
+        <span>Qasim's Personal Porn Website</span>
       </main>
       <ul className=" flex gap-4">
         <Link>Contact</Link>
